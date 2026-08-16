68fa0115f7a6bd1184c7562d8d9450c73b81a2a3
DimaPr7 <i.love.python.lt@gmail.com>
2026-08-16 23:45:19 +0300

    Fix API routing configuration with correct viewsets prefixes

--- cinema/urls.py
+++ cinema/urls.py
@@ -9,8 +9,8 @@ from cinema.views import (
 )
 
 router = DefaultRouter()
-router.register("actors", ActorViewSet)
 router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
